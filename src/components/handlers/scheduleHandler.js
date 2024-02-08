567dfefe28ded5747320ac837524e2c11cf62b2f
mkangoo <alexanderpavlov1408@gmail.com>
2024-02-08 12:34:59 +0300

    Refactor: Improve readability and organization
    - Extracted common functionality into separate functions for better modularity
    - Renamed variables and functions to follow consistent naming conventions
    - Removed redundant code blocks and replaced them with more concise alternatives
    - Added comments to clarify complex logic and enhance code documentation

--- src/components/handlers/scheduleHandler.js
+++ src/components/handlers/scheduleHandler.js
@@ -1,82 +1,93 @@
 import { getWeek } from '../definitionOfWeek.js'
 import { dataBase } from '../../data/database.js'
 
+const DAY_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const MESSAGE = '<b>🎉 Занятий нет, можно отдыхать.</b>'
 
-/*
+/**
  * Formats the lesson information for display to the user.
  *
  * @param {string} type - The type of the lesson, can be "Lecture" or "Practice".
  *
- * @param {object} event - An object containing information about the lesson,
- *  including start and end times, title, description, and classroom.
- *
  * @returns {string} - The formatted lesson information for display.
  */
-const formatLessons = (type, event) => {
-	const startTime = event.start.slice(11, 16)
-	const endTime = event.end.slice(11, 16)
-	const lessonType = type === 'Лекция' ? 'Лекция' : type === 'Практика' ? 'Практика' : ''
-	const lessonTitle = event.title || ''
-	const lessonDescription = event.description || ''
-	const audience = event.class || ''
-	return `\n\n⏰ ${startTime} - ${endTime}\n${lessonType}:\n<b>${lessonTitle}</b>\n${lessonDescription}\n${audience}`
+const formatLessons = (type, { start, end, title, description, audience }) => {
+	// Extract start and end times
+	const startTime = start.slice(11, 16)
+	const endTime = end.slice(11, 16)
+
+	// Retrieve lesson details or use default values if not provided
+	const lessonType = type
+	const lessonTitle = title || ''
+	const lessonDescription = description || ''
+	const audienceInfo = audience || ''
+
+	// Format lesson information
+	return `\n\n⏰ ${startTime} - ${endTime}\n${lessonType}:\n<b>${lessonTitle}</b>\n${lessonDescription}\n${audienceInfo}`
 }
 
-/*
+/**
  * Retrieves information about the lessons for the specified day.
  *
  * @param {string} targetDay - The target day for which lessons information is required.
- * @param {Array} lessons - An array containing the lessons data.
- * @returns {Array} - An array of formatted lesson information for the specified day.
+ *
+ * @returns {string} - A formatted string containing lesson information for the specified day.
  */
-const getLessonsInfo = (targetDay, dataLessons) => {
-	const lessonToday = dataLessons.find(days => days.day === targetDay)
-	if (!lessonToday || !lessonToday.events) return MESSAGE
+const getLessonsInfo = targetDay => {
+	// Find information about the lessons for the specified day
+	const lessonToday = dataBase.find(({ day, events }) => day === targetDay && events)
+	// If no lesson information is found for the specified day, return an error message
+	if (!lessonToday) return MESSAGE
+	// Get the current day of the week
 	const today = getWeek(new Date())
-	const lessonsInfo = lessonToday.events.map(event => {
-		const lection = event.lection || []
-		const practical = event.practical || []
-		const lessonType = lection.includes(today) ? 'Лекция' : practical.includes(today) ? 'Практика' : null
-		return lessonType ? formatLessons(lessonType, event) : null
-	})
-	return lessonsInfo.filter(info => info !== null)
+
+	// Format lesson information for the specified day
+	return (
+		lessonToday.events
+			// Iterate through all lessons for the specified day
+			.map(event => {
+				const { lection = [], practical = [] } = event
+				// Determine the type of lesson (lecture or practice) based on the current day of the week
+				const lessonType = lection.includes(today) ? 'Лекция' : practical.includes(today) ? 'Практика' : null
+				// If the lesson type is determined, format the lesson information; otherwise, return null
+				return lessonType ? formatLessons(lessonType, event) : null
+			})
+			// Remove all null values that may have occurred if the lesson type was not determined
+			.filter(info => info !== null)
+			// Concatenate the formatted lesson information into a single string
+			.join(' ')
+	)
 }
 
-/*
- * @param {number} shiftDay - Сдвиг для кнопки 'Расписание на завтра'
+/**
+ * Retrieves the schedule for a specific day, considering a shift if necessary.
+ *
+ * @param {number} shiftDay - The shift for the 'Schedule for tomorrow' button.
+ * @returns {string} - The formatted string containing lesson information for the specified day.
  */
 export const getScheduleForDay = (shiftDay = 0) => {
-	const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 	const currentDate = new Date()
 	const dayIndex = (currentDate.getDay() + shiftDay) % 7
 
-	if (dayIndex === 0 || dayIndex === 6) {
-		return MESSAGE
-	}
-	return getLessonsInfo(daysOfWeek[dayIndex], dataBase).join(' ')
-}
-
-const getDailySchedule = (dayOfWeek, data) => {
-	const lessonsInfo = getLessonsInfo(dayOfWeek, data)
-	return lessonsInfo.length > 0 ? lessonsInfo.join(' ') : MESSAGE
+	// Retrieve the schedule for the specified day based on the day index
+	return getLessonsInfo(DAY_OF_WEEK[dayIndex])
 }
 
 // Object containing the schedule for each day of the week
 export const schedule = {
 	monday: {
-		lessons: getDailySchedule('Monday', dataBase),
+		lessons: getLessonsInfo('Monday'),
 	},
 	tuesday: {
-		lessons: getDailySchedule('Tuesday', dataBase),
+		lessons: getLessonsInfo('Tuesday'),
 	},
 	wednesday: {
-		lessons: getDailySchedule('Wednesday', dataBase),
+		lessons: getLessonsInfo('Wednesday'),
 	},
 	thursday: {
-		lessons: getDailySchedule('Thursday', dataBase),
+		lessons: getLessonsInfo('Thursday'),
 	},
 	friday: {
-		lessons: getDailySchedule('Friday', dataBase),
+		lessons: getLessonsInfo('Friday'),
 	},
 }

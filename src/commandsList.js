9391958e475380c56635efcb792bf84d0bb206fb
mkangoo <alexanderpavlov1408@gmail.com>
2024-02-15 21:13:42 +0300

    refactor: moved bot commands to a separate file to improve structure

---
+++ src/commandsList.js
@@ -0,0 +1,6 @@
+export const commands = [
+	{ command: 'start', description: 'Запустить бота' },
+	{ command: 'schedule', description: 'Посмотреть расписание' },
+	{ command: 'week', description: 'Какая сейчас неделя' },
+	{ command: 'support', description: 'Написать в поддержку' },
+]

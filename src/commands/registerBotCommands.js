91f7ac1857ee27c941e416082e6cc1ff67f35b0a
mkangoo <alexanderpavlov1408@gmail.com>
2024-03-11 17:24:19 +0300

    chore: changing imports and adding async functions

--- src/commands/registerBotCommands.js
+++ src/commands/registerBotCommands.js
@@ -1,17 +1,18 @@
 import { greetingMessage, contactInfo } from './replyTemplates.js'
-import { createUrlBtn } from '../buttons/keyboards.js'
-import { getMainMenu } from '../inlineKeyboard/inlineKeyboard.js'
-import { getWeekNumber, isWeekEven } from '../utils/getWeekNumber.js'
+import { createUrlBtn } from '../buttons/createBotButtons.js'
+import { getInlineKeyboard } from '../inlineKeyboard/createInlineKeyboard.js'
+import { getWeekNumber } from '../utils/weekNumber.js'
+import { isCurrentWeekEven } from '../utils/isCurrentWeekEven.js'
 import { getWeekSchedule } from '../utils/scheduleHandler.js'
 
 /** @param {import('telegraf').Telegraf} bot*/
-export default bot => {
+export default async bot => {
 	const startHandler = ctx => {
 		const user = ctx.from
 		const userName = user.first_name
 		const pseudonym = user.username
 		const greetingName = userName || pseudonym
-		ctx.reply(`Привет, ${greetingName}!\n${greetingMessage}`, getMainMenu())
+		ctx.reply(`Привет, ${greetingName}!\n${greetingMessage}`, getInlineKeyboard())
 	}
 	/** @param {boolean} shiftWeek */
 	const getWeekScheduleHandler = (shiftWeek = false) => {
@@ -21,7 +22,7 @@ export default bot => {
 	}
 	const getWeekInfoHandler = ctx => {
 		const date = new Date()
-		const week = isWeekEven(date) ? 'Четная' : 'Нечетная'
+		const week = isCurrentWeekEven(date) ? 'Четная' : 'Нечетная'
 		ctx.replyWithHTML(`📆Сейчас <b>${week}</b> неделя\n📆Номер недели: <b>${getWeekNumber(new Date())}</b>`)
 	}
 	const getSupportHandler = ctx => ctx.reply(contactInfo)

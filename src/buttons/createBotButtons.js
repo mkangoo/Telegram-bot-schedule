4954b39840b0135ebe9e1e7a07e63d787a8c8e25
mkangoo <alexanderpavlov1408@gmail.com>
2024-03-11 17:23:01 +0300

    chore: moved the creation of buttons to a separate file

---
+++ src/buttons/createBotButtons.js
@@ -0,0 +1,15 @@
+import { Markup } from 'telegraf'
+
+export const getButtonDaysOfWeek = btnNextWeek => {
+	return Markup.inlineKeyboard([
+		[Markup.button.callback('Понедельник', btnNextWeek ? 'btnMondayNextWeek' : 'btnMonday')],
+		[Markup.button.callback('Вторник', btnNextWeek ? 'btnTuesdayNextWeek' : 'btnTuesday')],
+		[Markup.button.callback('Среда', btnNextWeek ? 'btnWednesdayNextWeek' : 'btnWednesday')],
+		[Markup.button.callback('Четверг', btnNextWeek ? 'btnThursdayNextWeek' : 'btnThursday')],
+		[Markup.button.callback('Пятница', btnNextWeek ? 'btnFridayNextWeek' : 'btnFriday')],
+	])
+}
+
+export const createUrlBtn = () => {
+	return Markup.inlineKeyboard([[Markup.button.url('Проверить на сайте', 'https://guu.ru/student/schedule/')]])
+}

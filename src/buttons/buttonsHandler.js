36ba19f05bff5cac5ddee1443c23b27538543018
mkangoo <alexanderpavlov1408@gmail.com>
2024-03-11 17:21:48 +0300

    feat: changed the processing of buttons and moved them to a separate file

---
+++ src/buttons/buttonsHandler.js
@@ -0,0 +1,24 @@
+import { getLessonsInfo } from '../utils/scheduleHandler.js'
+import { createUrlBtn } from './createBotButtons.js'
+
+const orderedWeekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+export default async bot => {
+	// eslint-disable-next-line no-shadow
+	const addButtonAction = async (bot, button, schedule) => {
+		bot.action(button, async ctx => {
+			try {
+				await ctx.answerCbQuery()
+				await ctx.replyWithHTML(schedule, createUrlBtn())
+			} catch (error) {
+				// eslint-disable-next-line no-console
+				console.error(error)
+			}
+		})
+	}
+
+	orderedWeekDays.forEach(day => {
+		addButtonAction(bot, `btn${day}`, getLessonsInfo(day))
+		addButtonAction(bot, `btn${day}NextWeek`, getLessonsInfo(day, true))
+	})
+}
